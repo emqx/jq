d43e1d8ac36d3d7a460880ac03d19611d8a4b755
Shawn <506895667@qq.com>
2022-04-07 10:51:27 +0800

    fix: use dynamic linked libraries for Mac OSX

--- c_src/get_version.sh
+++ c_src/get_version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
+grep '.*define.*(.*VERSION' "$1"/src/jq.erl | sed -E 's/.*VERSION.*,.*([[:digit:]]+).*/\1/'
 
-grep '.*define.*(.*VERSION' "$1"/src/jq.erl | sed -e 's/.*VERSION.*,.*\([[:digit:]]\+\).*/\1/'
